ef07078798d26de51d819ce75a59261887bc3183
emoacht <emotom@pobox.com>
2017-12-04 08:11:32 +0900

    Incremented minor version

--- Source/WlanProfileViewer/Properties/AssemblyInfo.cs
+++ Source/WlanProfileViewer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WLAN Profile Viewer")]
-[assembly: AssemblyCopyright("Copyright © 2015 EMO")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.8")]
-[assembly: AssemblyFileVersion("1.1.2.8")]
+[assembly: AssemblyVersion("1.2.10.0")]
+[assembly: AssemblyFileVersion("1.2.10.0")]
