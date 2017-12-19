47e8232d246e4a3e016bbde7bdbe5952fd4a4b02
emoacht <emotom@pobox.com>
2017-12-20 07:08:05 +0900

    Incremented minor version

--- Source/ReactivePropertyTest/Properties/AssemblyInfo.cs
+++ Source/ReactivePropertyTest/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReactiveProperty Test")]
-[assembly: AssemblyCopyright("Copyright © 2015 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
