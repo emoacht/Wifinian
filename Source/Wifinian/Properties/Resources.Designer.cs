5a09020b7ccc31386f914310336bcf19a7d685d5
emoacht <emotom@pobox.com>
2018-09-01 19:45:34 +0900

    Conform target framework to 4.6.2

--- Source/Wifinian/Properties/Resources.Designer.cs
+++ Source/Wifinian/Properties/Resources.Designer.cs
@@ -63,22 +63,26 @@ namespace Wifinian.Properties {
         /// <summary>
         ///   Looks up a localized string similar to ProjectSite=https://github.com/emoacht/Wifinian/blob/master/README.md
         ///License=https://github.com/emoacht/Wifinian/blob/master/LICENSE.txt
+        ///StartSignIn=Start on sign in
         ///Close=Close
-        ///RecordException=This app will terminate, because unexpected problem happened. Leave exception log on Desktop?.
+        ///NotWorkable=Can not obtain information on Wi-Fi connections.
+        ///RecordException=An unexpected problem occurred. Save exception log on Desktop?.
         /// </summary>
         public static string language_en {
             get {
                 return ResourceManager.GetString("language_en", resourceCulture);
             }
         }
-
-		/// <summary>
-		///   Looks up a localized string similar to ProjectSite=https://github.com/emoacht/Wifinian/blob/master/README_ja.md
-		///License=https://github.com/emoacht/Wifinian/blob/master/LICENSE.txt
-		///Close=閉じる
-		///RecordException=予想外の問題が起きたため、このアプリを終了します。例外のログをデスクトップに残しますか?.
-		/// </summary>
-		public static string language_ja {
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ProjectSite=https://github.com/emoacht/Wifinian/blob/master/README_ja.md
+        ///License=https://github.com/emoacht/Wifinian/blob/master/LICENSE.txt
+        ///StartSignIn=サインイン時に起動する
+        ///Close=閉じる
+        ///NotWorkable=Wi-Fi接続の情報を取得できません。
+        ///RecordException=予想外の問題が起きました。例外のログをデスクトップに残しますか？.
+        /// </summary>
+        public static string language_ja {
             get {
                 return ResourceManager.GetString("language_ja", resourceCulture);
             }
