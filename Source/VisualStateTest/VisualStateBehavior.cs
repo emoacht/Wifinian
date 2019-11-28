70b8620cdf05e76a2e4964fd7fb5cf33efe034ac
emoacht <emotom@pobox.com>
2019-11-29 04:03:27 +0900

    Migrate to Microsoft.Xaml.Behaviors.Wpf

--- Source/VisualStateTest/VisualStateBehavior.cs
+++ Source/VisualStateTest/VisualStateBehavior.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
-using System.Windows.Interactivity;
 using System.Windows.Media;
 using System.Windows.Threading;
+using Microsoft.Xaml.Behaviors;
 
 namespace VisualStateTest
 {
