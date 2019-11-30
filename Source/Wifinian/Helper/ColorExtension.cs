39a01d12679fe9da489fffab67d32f3e734b16d8
emoacht <emotom@pobox.com>
2019-12-01 02:24:54 +0900

    Add method to get opaque color

--- Source/Wifinian/Helper/ColorExtension.cs
+++ Source/Wifinian/Helper/ColorExtension.cs
@@ -8,11 +8,20 @@ using static System.Math;
 
 namespace Wifinian.Helper
 {
+	/// <summary>
+	/// Extension methods for <see cref="System.Windows.Media.Color"/>
+	/// </summary>
 	public static class ColorExtension
 	{
 		public static HsbColor ToAhsb(this Color source) =>
 			HsbColor.FromArgb(source);
 
+		/// <summary>
+		/// Gets Color brightened by a specified factor.
+		/// </summary>
+		/// <param name="source">Source Color</param>
+		/// <param name="factor">Factor</param>
+		/// <returns>Brightened Color</returns>
 		public static Color ToBrightened(this Color source, float factor)
 		{
 			if (factor <= 0F)
@@ -22,6 +31,19 @@ namespace Wifinian.Helper
 			bridgeColor.B = Min(1F, bridgeColor.B * factor);
 
 			return bridgeColor.ToArgb();
+		}
+
+		/// <summary>
+		/// Gets Color equivalent to translucent Color with white background.
+		/// </summary>
+		/// <param name="source">Source Color</param>
+		/// <returns>Opaque Color</returns>
+		public static Color ToOpaque(this Color source)
+		{
+			var alpha = source.A;
+			byte Compute(byte value) => (byte)(byte.MaxValue - (byte.MaxValue - value) * alpha / (float)byte.MaxValue);
+
+			return Color.FromRgb(Compute(source.R), Compute(source.G), Compute(source.B));
 		}
 	}
 }
