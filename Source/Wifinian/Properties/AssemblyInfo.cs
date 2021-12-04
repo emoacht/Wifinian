d31bd052611cbc1d9faefd16477c3b65ad1afce5
emoacht <emotom@pobox.com>
2021-12-05 00:57:58 +0900

    Increment minor version

--- Source/Wifinian/Properties/AssemblyInfo.cs
+++ Source/Wifinian/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
 [assembly: Guid("056ef371-bf6b-42c3-82b7-a113cafa5718")]
 [assembly: NeutralResourcesLanguage("en-US")]
